9dce4d1e9a8d06b371e6b462b894ce2805c9fbcb
Jose Carlos Pajuelo <josecarlos.2682@hotmail.com>
2023-05-11 02:38:23 -0500

    Nueva version - JCPSH

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
 import "./css/style.scss";
 
+// change appearance theme
+const toggleSwitch = document.querySelector(".switch-theme");
+const prefersDarkScheme =
+  window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)");
 const currentTheme = localStorage.getItem("theme");
+
 if (currentTheme === "dark") {
   document.body.classList.toggle("dark");
 }
 
-const prefresDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-const toggleSwitch = document.querySelector(".switch-theme");
-toggleSwitch.addEventListener("click", function () {
+function switchTheme() {
   let theme;
-  if (!prefresDarkScheme.matches) {
+  if (prefersDarkScheme.matches) {
     document.body.classList.toggle("dark");
-    theme = document.body.classList.contains("dark") ? "dark" : "";
+    theme = document.body.classList.contains("light") ? "light" : "dark";
+  } else {
+    document.body.classList.toggle("dark");
+    theme = document.body.classList.contains("dark") ? "dark" : "light";
   }
   localStorage.setItem("theme", theme);
+}
+
+prefersDarkScheme.addEventListener("change", switchTheme);
+toggleSwitch.addEventListener("click", function (e) {
+  e.preventDefault();
+  switchTheme();
 });
+
+// year and clock
+const date = document.querySelector(".date");
+const options = {
+  year: "numeric",
+};
+const dateNow = new Date();
+date.innerHTML = dateNow.toLocaleDateString("en-US", options);
+
+function clock() {
+  const dateNow = new Date();
+  const time = document.querySelector(".time");
+  time.innerHTML = dateNow.toLocaleTimeString("en-US", {
+    timeZone: "America/Lima",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+  });
+}
+window.addEventListener("DOMContentLoaded", clock);
+setInterval(clock, 1000);
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/",
-};
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+      output: {
+        entryFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+});
